b09e25d8119c6ffb8b31fa04fadfbdefbfef62a8
Reelap13 <116920397+Reelap13@users.noreply.github.com>
2023-01-12 18:58:11 +0300

    Updated player movement
    Updated player movement and camera movement to the player. New camera could zoom in and out and I did some minor improvements.

--- Anlaut Winter Jam 2023/Assets/Object/Camera/CameraController.cs
+++ Anlaut Winter Jam 2023/Assets/Object/Camera/CameraController.cs
@@ -7,18 +7,41 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private Vector3 _offset;
     [SerializeField] private float _followingSpeed = 5f;
+    [SerializeField] private float _minZoom = 0.5f;
+    [SerializeField] private float _maxZoom = 1f;
+    [SerializeField] private float _zoomSpeed = 0.05f;
+
+
 
     private Transform _cameraTransform;
+    private float _zoomLevel = 1;
 
     private void Start()
     {
         _cameraTransform = GetComponent<Transform>();
+        _cameraTransform.position = _playerTransform.position + _offset * _zoomLevel;
     }
 
     private void Update()
     {
-        Debug.Log(_offset);
-        Vector3 newCameraPosition = _playerTransform.position + _offset;
+        Vector3 newCameraPosition = _playerTransform.position + _offset * _zoomLevel;
         _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, newCameraPosition, _followingSpeed * Time.deltaTime);
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") != 0)
+        {
+            changeZoom();
+        }
+    }
+
+    private void changeZoom()
+    {
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0f)
+        {
+            _zoomLevel = _zoomLevel - _zoomSpeed >= _minZoom ? _zoomLevel - _zoomSpeed : _minZoom;
+        }
+        else
+        {
+            _zoomLevel = _zoomLevel + _zoomSpeed <= _maxZoom ? _zoomLevel + _zoomSpeed : _maxZoom;
+        }
     }
 }
